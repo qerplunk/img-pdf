fcbb874758119a1d5a46160de4bc1b2e54642e6c
qerplunk <aquinoedgar@outlook.com>
2024-06-15 00:21:21 -0700

    New feature: delete page selected

--- src/app/ImgPDF.tsx
+++ src/app/ImgPDF.tsx
@@ -9,289 +9,307 @@ const defWidth = 794;
 const defHeight = 1123;
 
 const CanvasComponent = ({
-  id,
-  sel,
-  setCanvases,
-  classname,
-  zoom,
+	id,
+	setCanvasSelected,
+	setCanvases,
+	classname,
+	zoom,
 }: {
-  id: string;
-  sel: Function;
-  setCanvases: Function;
-  classname: string;
-  zoom: number;
+	id: number;
+	setCanvasSelected: Function;
+	setCanvases: Function;
+	classname: string;
+	zoom: number;
 }) => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const fabricCanvasRef = useRef<fabric.Canvas | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const fabricCanvasRef = useRef<fabric.Canvas | null>(null);
 
-  useEffect(() => {
-    if (!fabricCanvasRef.current) {
-      const canvas = new fabric.Canvas(canvasRef.current, {
-        width: defWidth,
-        height: defHeight,
-        backgroundColor: "white",
-      });
-      fabricCanvasRef.current = canvas;
+	useEffect(() => {
+		if (!fabricCanvasRef.current) {
+			const canvas = new fabric.Canvas(canvasRef.current, {
+				width: defWidth,
+				height: defHeight,
+				backgroundColor: "white",
+			});
+			fabricCanvasRef.current = canvas;
 
-      setCanvases((prev: fabric.Canvas[]) =>
-        Array.isArray(prev) ? [...prev, canvas] : [canvas],
-      );
+			setCanvases((prev: fabric.Canvas[]) =>
+				Array.isArray(prev) ? [...prev, canvas] : [canvas],
+			);
 
-      sel(id, canvas);
+			canvas.on("mouse:down", () => {
+				setCanvasSelected(id, canvas);
+				canvas.renderAll();
+			});
+		}
+	}, [id, setCanvasSelected, setCanvases]);
 
-      canvas.on("mouse:down", () => {
-        sel(id, canvas);
-      });
-    }
-  }, [id, sel, setCanvases]);
+	useEffect(() => {
+		if (fabricCanvasRef.current) {
+			const canvas = fabricCanvasRef.current;
 
-  useEffect(() => {
-    if (fabricCanvasRef.current) {
-      const canvas = fabricCanvasRef.current;
+			canvas.setZoom(zoom);
+			canvas.setWidth(defWidth * canvas.getZoom());
+			canvas.setHeight(defHeight * canvas.getZoom());
+			canvas.renderAll();
+		}
+	}, [zoom]);
 
-      canvas.setZoom(zoom);
-      canvas.setWidth(defWidth * canvas.getZoom());
-      canvas.setHeight(defHeight * canvas.getZoom());
-      canvas.renderAll();
-    }
-  }, [zoom]);
-
-  return <canvas ref={canvasRef} id={id} className={classname} />;
+	return <canvas ref={canvasRef} id={id.toString()} className={classname} />;
 };
 
 export function ImgPDF() {
-  fabric.Object.prototype.transparentCorners = false;
-  fabric.Object.prototype.cornerColor = "#FF0000";
-  fabric.Object.prototype.cornerStyle = "rect";
-  fabric.Object.prototype.cornerStrokeColor = "#FFFFFF";
-  fabric.Object.prototype.cornerSize = 12;
-  fabric.Object.prototype.borderColor = "#FF0000";
-  fabric.Object.prototype.borderScaleFactor = 2;
+	fabric.Object.prototype.transparentCorners = false;
+	fabric.Object.prototype.cornerColor = "#FF0000";
+	fabric.Object.prototype.cornerStyle = "rect";
+	fabric.Object.prototype.cornerStrokeColor = "#FFFFFF";
+	fabric.Object.prototype.cornerSize = 12;
+	fabric.Object.prototype.borderColor = "#FF0000";
+	fabric.Object.prototype.borderScaleFactor = 2;
 
-  const [canvasZoom, setCanvasZoom] = useState<number>(0.8);
-  const [numCanvases, setNumCanvases] = useState(1);
-  const [c, setC] = useState<fabric.Canvas[]>();
+	const [canvasZoom, setCanvasZoom] = useState<number>(0.8);
+	const [c, setC] = useState<fabric.Canvas[]>();
 
-  const [selected, setSelected] = useState<{
-    id: string;
-    canvas: fabric.Canvas;
-  }>();
+	const [canvases, setCanvases] = useState<number[]>([0]);
+	const [nextBlankPage, setNextBlankPage] = useState<number>(1);
+	const [selected, setSelected] = useState<{
+		id: number;
+		canvas: fabric.Canvas;
+	}>();
 
-  const setCanvas = (id: string, canvas: fabric.Canvas) => {
-    setSelected({ id, canvas });
-  };
+	const setCanvasSelected = (id: number, canvas: fabric.Canvas) => {
+		setSelected({ id, canvas });
+	};
 
-  const handleAddImage =
-    (canvas: fabric.Canvas | undefined) => (event: any) => {
-      const file = event.target.files[0];
-      const reader = new FileReader();
+	const handleAddImage =
+		(canvas: fabric.Canvas | undefined) => (event: any) => {
+			if (selected?.id === -1 || selected?.id === undefined) {
+				event.target.value = "";
+				return;
+			}
 
-      reader.onload = function(f) {
-        const data = f.target?.result;
-        if (typeof data === "string") {
-          fabric.Image.fromURL(data, (img) => {
-            img.scaleToWidth(canvas!.getWidth() / (canvasZoom * 1.2));
-            if (img.getScaledHeight() > img.getScaledWidth()) {
-              img.scaleToHeight(canvas!.getHeight() / (canvasZoom * 1.2));
-            }
-            img.set({
-              left:
-                (canvas!.getWidth() / canvasZoom - img.getScaledWidth()) / 2,
-              top:
-                (canvas!.getHeight() / canvasZoom - img.getScaledHeight()) / 2,
-            });
-            canvas?.add(img);
-            canvas?.requestRenderAll();
-            canvas?.renderAll();
-          });
-        }
-      };
+			const file = event.target.files[0];
+			const reader = new FileReader();
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
+			reader.onload = function(f) {
+				const data = f.target?.result;
+				if (typeof data === "string") {
+					fabric.Image.fromURL(data, (img) => {
+						img.scaleToWidth(canvas!.getWidth() / (canvasZoom * 1.2));
+						if (img.getScaledHeight() > img.getScaledWidth()) {
+							img.scaleToHeight(canvas!.getHeight() / (canvasZoom * 1.2));
+						}
+						img.set({
+							left:
+								(canvas!.getWidth() / canvasZoom - img.getScaledWidth()) / 2,
+							top:
+								(canvas!.getHeight() / canvasZoom - img.getScaledHeight()) / 2,
+						});
+						canvas?.add(img);
+						canvas?.requestRenderAll();
+						canvas?.renderAll();
+					});
+				}
+			};
 
-      event.target.value = "";
-    };
+			if (file) {
+				reader.readAsDataURL(file);
+			}
 
-  return (
-    <div className="flex min-h-screen w-full bg-stone-900">
-      <div className="fixed left-0 top-0 z-10 w-44 rounded-br-2xl bg-cyan-900 p-2">
-        <div className="sticky flex flex-col">
-          <div className="pb-1">
-            <button
-              onClick={() => {
-                const activeOjb = selected?.canvas.getActiveObject();
-                if (activeOjb) {
-                  selected?.canvas.remove(activeOjb);
-                }
-              }}
-              className="flex h-full w-full flex-row items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
-            >
-              {"-"}
-              <Image
-                src={"/assets/trash.png"}
-                width={86}
-                height={86}
-                alt={"Trash"}
-                priority={true}
-              />
-            </button>
-          </div>
+			event.target.value = "";
+		};
 
-          <div className="pb-1">
-            <label
-              htmlFor="image-upload"
-              className="flex h-full w-full cursor-pointer items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
-            >
-              +
-              <Image
-                src={"/assets/new_image.png"}
-                width={86}
-                height={86}
-                alt={"New image"}
-                priority={true}
-              />
-            </label>
+	return (
+		<div className="flex min-h-screen w-full bg-stone-900">
+			<div className="fixed left-0 top-0 z-10 w-44 rounded-br-2xl bg-cyan-900 p-2">
+				<div className="sticky flex flex-col">
+					<div className="pb-1">
+						<button
+							onClick={() => {
+								const activeOjb = selected?.canvas.getActiveObject();
+								if (activeOjb) {
+									selected?.canvas.remove(activeOjb);
+								} else {
+									if (canvases.length > 1) {
+										const remove_canvas_id = selected?.id;
+										setCanvases((prev) =>
+											prev.filter((id) => {
+												return id != remove_canvas_id;
+											}),
+										);
 
-            <input
-              id="image-upload"
-              type="file"
-              onChange={handleAddImage(selected?.canvas)}
-              className="hidden"
-            />
-          </div>
+										setC((prev) =>
+											prev?.filter((_, index) => {
+												return index != remove_canvas_id;
+											}),
+										);
+										setCanvasSelected(-1, c?.at(0)!);
+									}
+								}
+							}}
+							className="flex h-full w-full flex-row items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
+						>
+							{"-"}
+							<Image
+								src={"/assets/trash.png"}
+								width={86}
+								height={86}
+								alt={"Trash"}
+								priority={true}
+							/>
+						</button>
+					</div>
 
-          <div className="pb-1">
-            <button
-              onClick={() => {
-                setNumCanvases((prev) => prev + 1);
-                selected?.canvas.discardActiveObject().renderAll();
-              }}
-              className="flex h-full w-full flex-row items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
-            >
-              +
-              <Image
-                src={"/assets/new_page.png"}
-                width={86}
-                height={86}
-                alt={"New page"}
-                priority={true}
-              />
-            </button>
-          </div>
+					<div className="pb-1">
+						<label
+							htmlFor="image-upload"
+							className="flex h-full w-full cursor-pointer items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
+						>
+							+
+							<Image
+								src={"/assets/new_image.png"}
+								width={86}
+								height={86}
+								alt={"New image"}
+								priority={true}
+							/>
+						</label>
 
-          <button
-            className="h-12 rounded-md border-2 border-white px-6 text-lg text-white hover:bg-red-500"
-            onClick={() => {
-              const prevZoom = canvasZoom;
-              const prevW = defWidth * canvasZoom;
-              const prevH = defHeight * canvasZoom;
+						<input
+							id="image-upload"
+							type="file"
+							onChange={handleAddImage(selected?.canvas)}
+							className="hidden"
+						/>
+					</div>
 
-              const pdf = new jsPDF("p", "mm", "a4");
-              c?.forEach((c, i) => {
-                if (i >= 1) {
-                  pdf.addPage();
-                }
+					<div className="pb-1">
+						{" "}
+						<button
+							onClick={() => {
+								selected?.canvas.discardActiveObject().renderAll();
+								setCanvases((prev) => [...prev, nextBlankPage]);
+								setNextBlankPage((prev) => prev + 1);
+							}}
+							className="flex h-full w-full flex-row items-center justify-center rounded-md border-2 border-white text-3xl text-white hover:bg-red-500"
+						>
+							+
+							<Image
+								src={"/assets/new_page.png"}
+								width={86}
+								height={86}
+								alt={"New page"}
+								priority={true}
+							/>
+						</button>
+					</div>
 
-                c.setZoom(1);
-                c.setWidth(defWidth);
-                c.setHeight(defHeight);
-                c.renderAll();
+					<button
+						className="h-12 rounded-md border-2 border-white px-6 text-lg text-white hover:bg-red-500"
+						onClick={() => {
+							const prevZoom = canvasZoom;
+							const prevW = defWidth * canvasZoom;
+							const prevH = defHeight * canvasZoom;
 
-                pdf.addImage(
-                  c.toDataURL({
-                    format: "image/jpeg",
-                    quality: 1.0,
-                    //multiplier: 1 / (canvasZoom + 0.2),
-                  }),
-                  "JPEG",
-                  0,
-                  0,
-                  pdf.internal.pageSize.getWidth(),
-                  pdf.internal.pageSize.getHeight(),
-                );
+							const pdf = new jsPDF("p", "mm", "a4");
+							c?.forEach((c, i) => {
+								if (i >= 1) {
+									pdf.addPage();
+								}
 
-                c.setZoom(prevZoom);
-                c.setWidth(prevW);
-                c.setHeight(prevH);
-                c.renderAll();
-              });
-              pdf.save("img_combined.pdf");
-            }}
-          >
-            {"IMG --> PDF"}
-          </button>
-          <div className="justify-center py-2">
-            <p className="text-center text-2xl text-white">Zoom</p>
-            <span className="flex justify-center gap-4">
-              <button
-                className="w-12 rounded-xl border-2 border-white text-3xl text-white hover:bg-red-500"
-                onClick={() => {
-                  setCanvasZoom((prev) => {
-                    let newZoom = prev + 0.1;
-                    if (newZoom > 2) {
-                      newZoom = 2;
-                    }
-                    return newZoom;
-                  });
-                }}
-              >
-                +
-              </button>
-              <button
-                className="w-12 rounded-xl border-2 border-white text-3xl text-white hover:bg-red-500"
-                onClick={() => {
-                  setCanvasZoom((prev) => {
-                    let newZoom = prev - 0.1;
-                    if (newZoom <= 0.3) {
-                      newZoom = 0.3;
-                    }
-                    return newZoom;
-                  });
-                }}
-              >
-                -
-              </button>
-            </span>
-          </div>
-        </div>
-        <p className="italic text-white">
-          If pages disappear, zoom in or out to refresh
-        </p>
-      </div>
+								c.setZoom(1);
+								c.setWidth(defWidth);
+								c.setHeight(defHeight);
+								c.renderAll();
 
-      <div className="flex w-full justify-center pt-10">
-        <div className="flex flex-col">
-          {[...Array(numCanvases)].map((_, i) => (
-            <div key={i}>
-              <div className="flex items-center">
-                <p
-                  className={`pr-1 font-bold text-red-500 ${selected?.id === `canvas${i}` ? "" : "invisible"} ${canvasZoom < 0.8 ? "text-2xl" : "text-4xl"}`}
-                >
-                  {"-->"}
-                </p>
-                <CanvasComponent
-                  id={`canvas${i}`}
-                  sel={setCanvas}
-                  setCanvases={setC}
-                  classname={`${selected?.id === `canvas${i}` ? "outline-dashed outline-3 outline-red-500 outline-offset-4" : ""}`}
-                  zoom={canvasZoom}
-                />
-                <p className="pl-4 text-xl text-gray-200">{i + 1}</p>
-              </div>
-              <br />
-            </div>
-          ))}
-        </div>
-      </div>
-      <Link
-        href={"https://github.com/qerplunk/img-pdf"}
-        target="_blank"
-        className="absolute right-0 top-0"
-      >
-        <p className="rounded-bl-md bg-cyan-900 p-1 text-white">GitHub</p>
-      </Link>
-    </div>
-  );
+								pdf.addImage(
+									c.toDataURL({
+										format: "image/jpeg",
+										quality: 1.0,
+										//multiplier: 1 / (canvasZoom + 0.2),
+									}),
+									"JPEG",
+									0,
+									0,
+									pdf.internal.pageSize.getWidth(),
+									pdf.internal.pageSize.getHeight(),
+								);
+
+								c.setZoom(prevZoom);
+								c.setWidth(prevW);
+								c.setHeight(prevH);
+								c.renderAll();
+							});
+							pdf.save("img_combined.pdf");
+						}}
+					>
+						{"IMG --> PDF"}
+					</button>
+					<div className="justify-center py-2">
+						<p className="text-center text-2xl text-white">Zoom</p>
+						<span className="flex justify-center gap-4">
+							<button
+								className="w-12 rounded-xl border-2 border-white text-3xl text-white hover:bg-red-500"
+								onClick={() => {
+									setCanvasZoom((prev) => {
+										let newZoom = prev + 0.1;
+										if (newZoom > 2) {
+											newZoom = 2;
+										}
+										return newZoom;
+									});
+								}}
+							>
+								+
+							</button>
+							<button
+								className="w-12 rounded-xl border-2 border-white text-3xl text-white hover:bg-red-500"
+								onClick={() => {
+									setCanvasZoom((prev) => {
+										let newZoom = prev - 0.1;
+										if (newZoom <= 0.3) {
+											newZoom = 0.3;
+										}
+										return newZoom;
+									});
+								}}
+							>
+								-
+							</button>
+						</span>
+					</div>
+				</div>
+				<p className="italic text-white">
+					If pages disappear, zoom in or out to refresh
+				</p>
+			</div>
+
+			<div className="flex w-full justify-center pt-10">
+				<div className="flex flex-col">
+					{canvases.map((value, _) => (
+						<div key={value} className="pb-4">
+							<CanvasComponent
+								id={value}
+								setCanvasSelected={setCanvasSelected}
+								setCanvases={setC}
+								classname={
+									selected?.id === value
+										? "outline-dashed outline-3 outline-red-500 outline-offset-4"
+										: ""
+								}
+								zoom={canvasZoom}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+			<Link
+				href={"https://github.com/qerplunk/img-pdf"}
+				target="_blank"
+				className="absolute right-0 top-0"
+			>
+				<p className="rounded-bl-md bg-cyan-900 p-1 text-white">GitHub</p>
+			</Link>
+		</div>
+	);
 }
